a6eaf6623153034198dce4b6e920b069c7f6df22
pg1219 <peter.gronseth@gmail.com>
2023-04-02 22:59:18 -0400

    late sunday push

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
-var apiKey = "565413d8c613629871121bd9ec189355"
+var search = document.getElementById('searchBtn')
+var cityDetail = document.getElementById('cityDetail')
 
-var currentCity;
+var APIKey = "565413d8c613629871121bd9ec189355"
+
+var city;
 
 function getWeather(data){
-    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
+    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+
     fetch(requestUrl)
     .then(function(response){
     return response.json()}
 )
-.then(function(data)) {
-    var currentWeather = $("currentWeather");
-    currentWeather.addClass("border")
+
+.then(function(data) {
+    console.log(data)
+
+    var cityDetailEl = $("#cityDetail");
+    cityDetailEl.addClass("border")
 
     var cityName = $("<h2>")
     cityName.text(currentCity)
-    currentWeather.append(cityName)
+    cityDetailEl.append(cityName)
 
-    var currentCityDate = dayjs().format("MMM D, YYYY");
+    var currentCityDate = data.current.dt; 
+    currentCityDate = moment.unix(currentCityDate).format("MM/DD/YYYY");
     var currentDateEl = $("<span>");
-    currentDateEl.text(currentCityDate)
-    cityName.append(currentDateEl)
+    currentDateEl.text("${currentCityDate}")
+    cityName.append(currentDateEl)}
 
- 
-}
+
+)}
+
+// fetchButton.addEventListener('click', getWeather);
