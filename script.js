a358936fae7f445da6f9b3be0fcb73491c922a47
pg1219 <peter.gronseth@gmail.com>
2023-04-07 00:33:48 -0400

    late friday push

--- script.js
+++ script.js
@@ -1,35 +1,67 @@
-var search = document.getElementById('searchBtn')
-var cityDetail = document.getElementById('cityDetail')
+var searchEl = document.getElementById("#searchResults");
+var cityDetail = document.getElementById("cityDetail");
 
-var APIKey = "565413d8c613629871121bd9ec189355"
+var APIKey = "565413d8c613629871121bd9ec189355";
+var currentCity = "London"; 
 
-var city;
-
-function getWeather(data){
-    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
-
-    fetch(requestUrl)
-    .then(function(response){
-    return response.json()}
-)
-
-.then(function(data) {
-    console.log(data)
-
-    var cityDetailEl = $("#cityDetail");
-    cityDetailEl.addClass("border")
-
-    var cityName = $("<h2>")
-    cityName.text(currentCity)
-    cityDetailEl.append(cityName)
-
-    var currentCityDate = data.current.dt; 
-    currentCityDate = moment.unix(currentCityDate).format("MM/DD/YYYY");
-    var currentDateEl = $("<span>");
-    currentDateEl.text("${currentCityDate}")
-    cityName.append(currentDateEl)}
+function getCoords() {
+ 
+var coordUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    currentCity +
+    "&appid=" +
+    APIKey;
+  fetch(coordUrl)
+  .then(function (response) {
+    return response.json()
+  })
+  .then(function(reply){
+    console.log(reply.coord.lon)
+    localStorage.setItem("lon", reply.coord.lon)
+    console.log(reply.coord.lat)
+    localStorage.setItem("lat", reply.coord.lat)
+  })
+}
+getCoords()
 
 
-)}
+
+// function citySearch(event){
+//     event.preventDefault();
+
+//     var searchInput = document.querySelector('#search-input').value;
+//     console.log(searchInput)
+
+//     var queryString = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&appid=" + APIKey;
+
+//     location.assign(queryString);
+// }
+// searchEl.addEventListener('click', citySearch);
+
+// function getWeather(data){
+//     var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=565413d8c613629871121bd9ec189355";
+
+//     fetch(requestUrl)
+//     .then(function(response){
+//     return response.json()}
+// )
+
+// .then(function(data) {
+//     console.log(data)
+
+//     var cityDetailEl = $("#cityDetail");
+//     cityDetailEl.addClass("border")
+
+//     var cityName = $("<h2>")
+//     cityName.text(currentCity)
+//     cityDetailEl.append(cityName)
+
+//     var currentCityDate = data.current.dt;
+//     currentCityDate = moment.unix(currentCityDate).format("MM/DD/YYYY");
+//     var currentDateEl = $("<span>");
+//     currentDateEl.text("${currentCityDate}")
+//     cityName.append(currentDateEl)}
+
+// )}
 
 // fetchButton.addEventListener('click', getWeather);
