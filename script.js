022821398fd413cce55c75f4562152d23c562dc1
pg1219 <peter.gronseth@gmail.com>
2023-04-15 17:48:51 -0400

    sat evening push

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var APIKey = "565413d8c613629871121bd9ec189355";
-var cityInput = document.getElementById("#city-input");
-var searchBtn = document.getElementById("#search-btn");
+var cityInput = document.getElementById("city-input");
+var searchBtn = document.getElementById("search-btn");
 var cityDetail = document.getElementById("current-conditions");
 var currentCity = localStorage.getItem("city");
 var lat = localStorage.getItem("lat");
@@ -12,45 +12,42 @@ var humidity = localStorage.getItem("humidity");
 var wind = localStorage.getItem("wind");
 
 // store search value
-$(document).ready(function () {
-  $("#search-btn").click(function () {
-    var city = $("#city-input").val();
-    localStorage.setItem("city", city);
-  });
-});
-
-
-function getCoords() {
-  var coordUrl =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    currentCity +
-    "&appid=" +
-    APIKey;
-
-  console.log(coordUrl);
-
-  fetch(coordUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      var cityData = {
-        city: currentCity,
-        lon: data.coord.lon,
-        lat: data.coord.lat,
-        date: data.dt,
-      };
-
-      localStorage.setItem("lon", data.coord.lon);
-      localStorage.setItem("lat", data.coord.lat);
-      localStorage.setItem("date", data.dt);
-
-      console.log(cityData);
-    });
-}
-getCoords();
 
 function getWeather() {
+  $(document).ready(function () {
+    $("#search-btn").click(function () {
+      var city = $("#city-input").val();
+      localStorage.setItem("city", city);
+    });
+  });
+
+  function getCoords() {
+    var coordUrl =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      currentCity +
+      "&appid=" +
+      APIKey;
+
+    console.log(coordUrl);
+
+    fetch(coordUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        var cityData = {
+          lon: data.coord.lon,
+          lat: data.coord.lat,
+        };
+
+        localStorage.setItem("lon", data.coord.lon);
+        localStorage.setItem("lat", data.coord.lat);
+
+        console.log(cityData);
+      });
+  }
+  getCoords();
+
   var requestUrl =
     "https://api.openweathermap.org/data/2.5/weather?lat=" +
     lat +
@@ -121,10 +118,8 @@ function getWeather() {
   var fiveDayHeader = $("<h2>");
   fiveDayHeader.text("5-Day Forecast:");
   fiveDayForecastHeader.append(fiveDayHeader);
-  
-  var fiveDayForecastEl = $("#five-day")
 
-
+  var fiveDayForecastEl = $("#five-day");
 
   var fiveDayUrl =
     "https://api.openweathermap.org/data/2.5/forecast?lat=" +
@@ -144,46 +139,44 @@ function getWeather() {
 
     .then(function (data) {
       for (var i = 1; i <= 5; i++) {
-      var dateFive;
-      var tempFive;
-      var iconFive;
-      var windFive;
-      var humidityFive;
+        var dateFive;
+        var tempFive;
+        var iconFive;
+        var windFive;
+        var humidityFive;
 
-      dateFive = data.list[i].dt;
-      dateFive = dayjs.unix(dateFive).format("MM/DD/YYYY");
+        dateFive = data.list[i].dt;
+        dateFive = dayjs.unix(dateFive).format("MM/DD/YYYY");
 
-      console.log(dateFive);
+        console.log(dateFive);
 
-      tempFive = data.list[i].main.temp;
-      iconFive = data.list[i].weather[0].icon;
-      windFive = data.list[i].wind.speed;
-      humidityFive = data.list[i].main.humidity;
+        tempFive = data.list[i].main.temp;
+        iconFive = data.list[i].weather[0].icon;
+        windFive = data.list[i].wind.speed;
+        humidityFive = data.list[i].main.humidity;
 
-      console.log(tempFive);
-      console.log(iconFive);
-      console.log(windFive);
-      console.log(humidityFive);
+        console.log(tempFive);
+        console.log(iconFive);
+        console.log(windFive);
+        console.log(humidityFive);
 
-      var card = document.createElement("div");
-      card.classList.add("card", "col-2");
+        var card = document.createElement("div");
+        card.classList.add("card", "col-2");
 
-      var cardBody = document.createElement("div");
-      cardBody.classList.add("card-body");
-      cardBody.innerHTML = 
-      `<h4>${dateFive}</h4>
+        var cardBody = document.createElement("div");
+        cardBody.classList.add("card-body");
+        cardBody.innerHTML = `<h4>${dateFive}</h4>
       <img src= "http://openweathermap.org/img/wn/${iconFive}.png"></img><br>
       ${tempFive}°F<br>
       ${windFive} MPH <br>
       ${humidityFive}%`;
 
-      card.appendChild(cardBody);
-      fiveDayForecastEl.append(card);
-  }});
-
+        card.appendChild(cardBody);
+        fiveDayForecastEl.append(card);
+      }
+    });
 }
 
 getWeather();
 
-
-searchBtn.addEventListener('click', getWeather);
+searchBtn.addEventListener("click", getWeather);
